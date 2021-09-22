6c9c55590729857e39c3c418f0cd8087b13cf9d6
panoramine <camila.c.a.salles@gmail.com>
2021-09-22 16:14:35 -0700

    pair programming exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let answerArray = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      let tmpArray = [];
+  
+      for (let j = 0; j < matrix.length; j++) {
+        tmpArray.push(matrix[j][i]);
+      }
+  
+      answerArray.push(tmpArray);
     }
-}
+  
+    return answerArray;
+  };
 
+
+const findWord = (letters, word) => {
+    const newWords = letters.map(ls => ls.join(''));
+    for (const letters of newWords) {
+        if (letters.includes(word)) return true;
+    }
+};
+
+
+const wordSearch = (letters, word) => {
+    if (findWord(letters, word)) {
+        return true;
+    } else if (findWord(transpose(letters), word)) {
+        return true;
+    }
+    return false;
+};
 module.exports = wordSearch
+
